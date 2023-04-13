c4257cecb95610ef6ed5f04321590af4b9fde17d
heloisasantana <heloisasantana.silva@hotmail.com>
2023-04-12 23:37:15 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        sys.stderr.write("Formato inválido")
+    else:
+        try:
+            with open(path_file) as file:
+                return file.read().split("\n")
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
