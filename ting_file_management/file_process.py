f8443d58899099eb3d20c5792e73af22b56b0aa9
heloisasantana <heloisasantana.silva@hotmail.com>
2023-04-13 00:00:36 -0300

    Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -16,7 +16,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) > 0:
+        name_file = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {name_file} removido com sucesso\n")
+    else:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
