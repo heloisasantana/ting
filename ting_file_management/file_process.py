46e82ab588f3b131f3522a34ecbf14b2c8944168
heloisasantana <heloisasantana.silva@hotmail.com>
2023-04-12 23:58:22 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance.queue:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+    new_object = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    instance.enqueue(new_object)
+    sys.stdout.write(str(new_object))
 
 
 def remove(instance):
