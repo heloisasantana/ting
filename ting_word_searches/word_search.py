75c428323d69b04a3eb8f52d63134f8007ed9d88
heloisasantana <heloisasantana.silva@hotmail.com>
2023-04-13 00:45:27 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    final_list = list()
+    for item in instance.queue:
+        occurrences_info = []
+        new_object = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": occurrences_info,
+            }
+        for i, row in enumerate(item["linhas_do_arquivo"]):
+            if word in row.lower():
+                occurrences_info.append({"linha": i + 1})
+        if len(occurrences_info):
+            final_list.append(new_object)
+    return final_list
 
 
 def search_by_word(word, instance):
